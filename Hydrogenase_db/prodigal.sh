f452678c1a0af3dc7a2442a0410cca7e51d39493
XiangZhouCAS <147217990+XiangZhouCAS@users.noreply.github.com>
2026-09-10 11:17:56 +0800

    Delete Hydrogenase_db directory

--- Hydrogenase_db/prodigal.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p tmp_workfolder
-cat test.fa | \
-parallel -j 200 --block 999k --recstart '>' --pipe \
-"prodigal -p meta -a tmp_workfolder/tmp_{#}.faa -d tmp_workfolder/tmp_{#}.ffn -o tmp_workfolder/tmp_{#}.gff"
-cat tmp_workfolder/*.faa > test.faa
-rm -r tmp_workfolder
